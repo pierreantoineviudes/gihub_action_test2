d46de9f0dfd96e78465a91d60a4d581fd88c6e8b
Your Name <pierre.antoine.viudes@gmail.com>
2024-11-21 13:34:16 +0100

    add test sample

---
+++ test_sample.py
@@ -0,0 +1,14 @@
+"""
+    function to test module hello world
+"""
+
+import sys
+
+print(sys.path)
+
+
+
+def test_hello_world():
+    """testing hello world"""
+    # assert hello_world() == "Hello world, version 1.0.0 !"
+    pass
